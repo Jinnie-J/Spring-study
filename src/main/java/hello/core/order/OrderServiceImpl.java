3789eb9bb8dfb4544609547e446a9acec7b36826
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-02-11 16:53:12 +0900

    Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -21,4 +21,9 @@ public class OrderServiceImpl implements  OrderService{
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    //singleton 테스트 용도도
+   public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
