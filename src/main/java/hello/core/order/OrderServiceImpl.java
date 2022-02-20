c9d77946d6aeda849482753c1a7bc4c77849b45a
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-02-20 22:06:47 +0900

    롬복, 2개 이상의 빈, 자동 수동의 올바른 실무 운영 기준

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,16 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements  OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -26,7 +22,7 @@ public class OrderServiceImpl implements  OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    //singleton 테스트 용도도
+    //singleton 테스트 용도
    public MemberRepository getMemberRepository(){
         return memberRepository;
     }
